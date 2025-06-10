496e8bb1e1d5ab8803a2e8cd8128acb831b2890e
taek2222 <oot309@daum.net>
2025-06-10 16:10:00 +0900

    chore: 기본 Data SQL 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+insert into CIVIL_WAR_SCHEDULE (start_schedule, end_schedule)
+values ('2025-06-11T10:00:00', '2025-06-11T15:00:00'),
+       ('2025-06-11T15:00:00', '2025-06-11T20:00:00');
+
+insert into CREW(name, tier, line)
+values ('비타', 'CHALLENGER', 'MID'),
+       ('체체', 'IRON4', 'JUNGLE'),
+       ('백터', 'IRON4', 'SUPPORT'),
+       ('백엔드라고', 'DIAMOND2', 'ADC'),
+       ('머피', 'GRANDMASTER', 'TOP')
+
+
+
