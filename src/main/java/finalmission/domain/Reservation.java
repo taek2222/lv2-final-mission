9a11f24ccf29afbabd1da2ece78aa8ce15a153c6
taek2222 <oot309@daum.net>
2025-06-18 00:09:50 +0900

    feat: 연관 관계 지연 로딩 추가

--- src/main/java/finalmission/domain/Reservation.java
+++ src/main/java/finalmission/domain/Reservation.java
@@ -2,6 +2,7 @@ package finalmission.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -23,11 +24,11 @@ public class Reservation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "room_id")
     private Room room;
 
