463d711370d1299a90be189cc65b5c48e1365301
taek2222 <oot309@daum.net>
2025-06-17 15:27:23 +0900

    test: 외부 API 의존 통합테스트 Disabled 처리

--- src/test/java/finalmission/infrastructure/CoolSmsClientTest.java
+++ src/test/java/finalmission/infrastructure/CoolSmsClientTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 
 import finalmission.domain.Reservation;
 import finalmission.repository.ReservationRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +19,7 @@ class CoolSmsClientTest {
     private ReservationRepository reservationRepository;
 
     @Test
+    @Disabled
     void 회의실_예약_성공_문자를_전송한다() {
         // given
         Reservation reservation = reservationRepository.findById(1L)
