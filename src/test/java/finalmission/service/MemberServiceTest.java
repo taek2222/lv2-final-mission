57ca22707cd47129034b735eb67afb7e08fbaccd
taek2222 <oot309@daum.net>
2025-06-17 23:29:00 +0900

    test: 중복되는 이메일인 경우 예외 테스트 작성

---
+++ src/test/java/finalmission/service/MemberServiceTest.java
@@ -0,0 +1,33 @@
+package finalmission.service;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import finalmission.controller.dto.MemberSignupRequest;
+import finalmission.domain.Member;
+import finalmission.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private MemberService memberService;
+
+    @Test
+    void 회원가입시_중복되는_이메일인_경우_예외가_발생한다() {
+        // given
+        memberRepository.save(new Member("test1", "duplicate@email.com", "password", "01012345676"));
+
+        MemberSignupRequest request = new MemberSignupRequest("test2", "duplicate@email.com", "password",
+                "01056781234");
+
+        // when && then
+        assertThatCode(() -> memberService.signup(request))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복된 이메일이 존재합니다. 다른 이메일을 이용해 주세요.");
+    }
+}
