aeaa88ff87efd9ab077ca511b58cc488e6a5fb22
taek2222 <oot309@daum.net>
2025-06-17 15:26:59 +0900

    test: 회의실 예약 성공 문자 전송 테스트 작성

---
+++ src/test/java/finalmission/infrastructure/CoolSmsClientTest.java
@@ -0,0 +1,30 @@
+package finalmission.infrastructure;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import finalmission.domain.Reservation;
+import finalmission.repository.ReservationRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolSmsClientTest {
+
+    @Autowired
+    private CoolSmsClient coolSmsClient;
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    @Test
+    void 회의실_예약_성공_문자를_전송한다() {
+        // given
+        Reservation reservation = reservationRepository.findById(1L)
+                .orElseThrow();
+
+        // when && then
+        assertThatCode(() -> coolSmsClient.sendReservationSms(reservation))
+                .doesNotThrowAnyException();
+    }
+}
